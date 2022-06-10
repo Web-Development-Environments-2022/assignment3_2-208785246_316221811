d6fc766ccb03339f4061655aa70d0507bc619cc0
noashpack <shpack@post.bgu.ac.il>
2022-06-10 15:07:56 +0300

    family and last viewed

--- routes/user.js
+++ routes/user.js
@@ -52,7 +52,56 @@ router.get('/favorites', async (req,res,next) => {
   }
 });
 
+/**
+ * This path gets body with owner, time, gradients, preparation, image url and save this recipe in the family recipes list of the logged-in user
+ */
+router.post("/family", async (req, res, next) => {
+  try {
+    const user_id = req.session.user_id;
+    let recipe_details = {
+      owner: req.body.owner,
+      time: req.body.time,
+      gradients: req.body.gradients,
+      preparation: req.body.preparation,
+      image_url: req.body.image_url,
+    }
+    await DButils.execQuery(
+      `INSERT INTO familyrecipes VALUES ('${user_id}','${recipe_details.owner}', '${recipe_details.time}', '${recipe_details.gradients}',
+      '${recipe_details.preparation}', '${recipe_details.image_url}')`
+    );
+    res.status(201).send({ message: "family recipe created", success: true });
+  } catch (error) {
+    next(error);
+  }
+});
 
+/**
+ * This path returns the family recipes that were saved by the logged-in user
+ */
+ router.get('/family', async (req,res,next) => {
+  try{
+    const user_id = req.session.user_id;
+    let family_recipes = {};
+    const recipes_id = await user_utils.getFamilyRecipes(user_id);
+    let recipes_id_array = [];
+    recipes_id.map((element) => recipes_id_array.push(element.recipe_id)); //extracting the recipe ids into array
+    const results = await recipe_utils.getRecipesPreview(recipes_id_array);
+    res.status(200).send(results);
+  } catch(error){
+    next(error); 
+  }
+});
+
+router.get("/ThreeLastRecipes", async (req, res, next) => {
+  try {
+    const user_id = req.session.user_id;
+    let recipesData = await user_utils.getThreeLastRecipesIds(user_id);
+    res.send(recipesData);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
 
 
 module.exports = router;
